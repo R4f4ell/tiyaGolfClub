e31033fe508eb2d9b270418aebedee6b06d6c3bc
Rafael <132296553+R4f4ell@users.noreply.github.com>
2025-07-12 23:17:27 -0300

    Em Andamento
    Organização das sessões About, Wave e Membership para desktop.

--- src/components/about/AboutTiya.jsx
+++ src/components/about/AboutTiya.jsx
@@ -6,14 +6,16 @@ const AboutTiya = () => {
   return (
     <section className="about-tiya" id='about-tiya'>
       <p className="about-tiya__subtitle">About Tiya</p>
-      <h2 className="about-tiya__title">Tiya Club History</h2>
-      <p className="about-tiya__description">
-        <p>Fundada em 1998, a Tiya Golf Club nasceu da paixão pelo golfe e pelo estilo de vida que o esporte representa. Localizado em meio à natureza, nosso clube oferece muito mais do que campos profissionais — é um espaço pensado para quem busca excelência, convivência e bem-estar.</p>
+      <div>
+        <h2 className="about-tiya__title">Tiya Club History</h2>
+        <p className="about-tiya__description">
+          <p>Fundada em 1998, a Tiya Golf Club nasceu da paixão pelo golfe e pelo estilo de vida que o esporte representa. Localizado em meio à natureza, nosso clube oferece muito mais do que campos profissionais — é um espaço pensado para quem busca excelência, convivência e bem-estar.</p>
         
-        <p>Ao longo dos anos, nos tornamos referência em experiências de alto padrão, combinando tradição e modernidade. Nossos membros desfrutam de estrutura completa, eventos exclusivos, programas de treinamento e um ambiente acolhedor, seja para atletas experientes ou iniciantes.</p>
+          <p>Ao longo dos anos, nos tornamos referência em experiências de alto padrão, combinando tradição e modernidade. Nossos membros desfrutam de estrutura completa, eventos exclusivos, programas de treinamento e um ambiente acolhedor, seja para atletas experientes ou iniciantes.</p>
         
-        <p>Na Tiya, o golfe é só o começo de uma jornada inspiradora.</p>
-      </p>
+          <p>Na Tiya, o golfe é só o começo de uma jornada inspiradora.</p>
+        </p>
+      </div>
 
       <div className="about-tiya__team">
         <div className="member">
